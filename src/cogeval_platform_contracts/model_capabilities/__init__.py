6aac931d4714b662579659768fc36ac981e4c218
liuming <liuming@123.com>
2026-06-29 13:17:24 +0800

    Add model capability built-in account contract

--- src/cogeval_platform_contracts/model_capabilities/__init__.py
+++ src/cogeval_platform_contracts/model_capabilities/__init__.py
@@ -2,18 +2,36 @@
 
 from cogeval_platform_contracts.model_capabilities.resources import list_fixtures, load_fixture, load_schema
 from cogeval_platform_contracts.model_capabilities.v1 import (
+    BuiltInAccountCapability,
+    BuiltInAccountAgentId,
+    InterfaceThinkingEffortSurface,
     MODEL_CAPABILITY_CATALOG_SCHEMA,
     ModelCapability,
     ModelCapabilityCatalog,
+    ModelInterfaceAdapterPolicy,
+    ModelInterfaceCapability,
+    ModelInterfaceThinkingEffortCapability,
     ModelThinkingEffortCapability,
+    ProviderInterfaceCapabilityContract,
+    ProviderInterfaceId,
+    ThinkingEffortPlatformValue,
     validate_model_capability_catalog,
 )
 
 __all__ = [
+    "BuiltInAccountCapability",
+    "BuiltInAccountAgentId",
+    "InterfaceThinkingEffortSurface",
     "MODEL_CAPABILITY_CATALOG_SCHEMA",
     "ModelCapability",
     "ModelCapabilityCatalog",
+    "ModelInterfaceAdapterPolicy",
+    "ModelInterfaceCapability",
+    "ModelInterfaceThinkingEffortCapability",
     "ModelThinkingEffortCapability",
+    "ProviderInterfaceCapabilityContract",
+    "ProviderInterfaceId",
+    "ThinkingEffortPlatformValue",
     "list_fixtures",
     "load_fixture",
     "load_schema",
